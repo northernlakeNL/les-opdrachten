28261e4b8b089b928d8d37eecc8e2cfc1d0b4320
Northie <55920202+northernlakeNL@users.noreply.github.com>
2022-02-11 13:29:01 +0100

    work work work

--- Blok C/1_12.cpp
+++ Blok C/1_12.cpp
@@ -4,18 +4,17 @@ using namespace std;
 
 int main(){
     int max;
-    cout << "How Big? \n";
+    cout << "Hoe Groot? ";
     cin >> max;
-    std::string stars = "\n*";
+    std::string stars = "    \n";
     char ch = '*';
-    std::cout << stars;
     for ( int i = 0; i < max; ++i){
         if (i < max){
             stars.append(1, ch);
             std::cout << stars;
         }
     }
-    max = max + 2;
+    max = max + 1;
     for ( int i = max - 1; i < max; i--){
         if (i > 0){
             stars.erase(i, ch);
