9fba957e38781cfd5a1bc5c1f8d68d137e6e9dc6
Northie <55920202+northernlakeNL@users.noreply.github.com>
2022-02-22 13:32:43 +0100

    3.6

--- Blok C/W3/3_12.cpp
+++ Blok C/W3/3_12.cpp
@@ -1,7 +1,30 @@
 //
 // Created by tomno on 20/02/2022.
 //
+#include "iostream"
+#include "vector"
+using namespace std;
+
+vector<vector<int>> transpose(vector<vector<int>> & matrix){
+    int temp;
+    for (int x = 0; x<matrix.size(); ++x){
+        for (int y = 0; matrix[x].size(); ++y){
+
+        }
+    }
+    return matrix;
+}
+
 
 void drie_twaalf(){
-
+    vector<vector<int>> team = { {1,2,3},
+                                 {4,5,6},
+                                 {7,8,9}};
+    vector<vector<int>> swapped = transpose(team);
+    for (int x = 0; x < swapped.size();++x){
+        for (int y = 0; y < swapped[x].size(); ++y){
+            cout << swapped[x][y]<<",";
+        }
+        cout << endl;
+    }
 }
