05c4f7818d212bf9562939533896e769dd734ac6
Northie <55920202+northernlakeNL@users.noreply.github.com>
2022-03-08 14:02:03 +0100

    3 12 dingen

--- Blok C/W3/3_12.cpp
+++ Blok C/W3/3_12.cpp
@@ -3,15 +3,14 @@
 using namespace std;
 
 vector<vector<int>> transpose(vector<vector<int>> & matrix){
-    vector<int> matrix2;
+    vector<int> matrix_temp1;
+    vector<int> matrix_temp2;
+    vector<int> matrix_temp3;
+    vector<vector<int>> matrix2;
     for (int x = 0; x<matrix.size(); ++x){
         for (int y = 0; y<matrix[x].size(); ++y) {
-            cout << matrix[x][y];
-//            for (vector<int> c = {};
-//                matrix2.insert(matrix2.end(), c);
-//            }
         }
-
+            cout << endl;
     }
     return matrix;
 }
@@ -21,10 +20,10 @@ void drie_twaalf(){
                                  {4,5,6},
                                  {7,8,9}};
     vector<vector<int>> swapped = transpose(team);
-    for (int x = 0; x < swapped.size();++x){
-        for (int y = 0; y < swapped[x].size(); ++y){
-            cout << swapped[x][y]<<",";
-        }
-        cout << endl;
-    }
+//    for (int x = 0; x < swapped.size();++x){
+//        for (int y = 0; y < swapped[x].size(); ++y){
+//            cout << swapped[x][y]<<",";
+//        }
+//        cout << endl;
+//    }
 }
