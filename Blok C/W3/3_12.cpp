c65d242a162beeeab803fdd4e378c19a2c97b2c3
northernlakeNL <55920202+northernlakeNL@users.noreply.github.com>
2022-03-13 12:31:32 +0100

    3 6

--- Blok C/W3/3_12.cpp
+++ Blok C/W3/3_12.cpp
@@ -3,14 +3,22 @@
 using namespace std;
 
 vector<vector<int>> transpose(vector<vector<int>> & matrix){
-    int temp;
-    for (int x = 0; x<matrix.size(); ++x){
-        for (int y = 0; y<matrix[x].size(); ++y){
-            temp = matrix[y][x];
-            matrix[x][y] = matrix[y][x];
-            matrix[x][y] = temp;
+    int temp = 0;
+    vector<int> used = {1};
+    for (int x = 0; x < matrix.size(); ++x) {
+        for (int y = 0; y < matrix[x].size(); ++y) {
+            for (int z = 0; z < used.size(); ++z) {
+                do{
+                    temp = matrix[x][y];
+                    used.push_back(temp);
+                    matrix[x][y] = matrix[y][x];
+                    matrix[y][x] = temp;
+                }
+                while(temp != used[z]);
+                }
+            continue;
+            }
         }
-    }
     return matrix;
 }
 
--- Blok C/W3/3_6.cpp
+++ Blok C/W3/3_6.cpp
@@ -6,29 +6,28 @@
 
 using namespace std;
 
-float average_list(vector<int> list){           // Het gemiddelde uitrekenen van de een lijst
+float average_list(vector<float> list){           // Het gemiddelde uitrekenen van de een lijst
     float total = list[0];
     for (int x = 1; x < list.size(); ++x){
         total = total + list[x];            // alles bij elkaar optellen
     }
     float av = total/(0+list.size());       // delen door de lengte om het gemiddelde te krijgen
     return av;
-    return 0;
 }
 
-float average(vector<vector<int>> list){
-    vector<int> sum = {};
+float average(vector<vector<float>> list){
+    vector<float> sum = {};
     for (int x = 0; x < list.size(); ++x){      // lijsten individualiseren
-         int av = average_list(list[x]);
+         float av = average_list(list[x]);
          sum.insert(sum.end(), av);
     }
-    int total_av = average_list(sum); // het gemiddelde van alle lijsten optellen
+    float total_av = average_list(sum); // het gemiddelde van alle lijsten optellen
     return total_av;
 }
 
 
 void drie_zes(){
-    vector<vector<int>> random = {  {7, 82, 52, 96, 65, 33, 16, 23,18, 3, 6},           // willekeurig gekozen nummers
+    vector<vector<float>> random = {  {7, 82, 52, 96, 65, 33, 16, 23,18, 3, 6},           // willekeurig gekozen nummers
                                     {14, 63, 67, 29, 85, 51, 6, 5, 89, 45, 1},
                                     {34, 83, 80, 97, 36, 45, 20, 13, 30, 68, 75},
                                     {22, 7, 4, 61, -18, 58, -52, -56,-36, -93, 62},
