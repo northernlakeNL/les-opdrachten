2aeb7467f91b91b5636fc9856bc019a6e1701616
northernlakeNL <55920202+northernlakeNL@users.noreply.github.com>
2022-03-17 21:17:43 +0100

    5 8 begin

--- Blok C/W5/5_6.cpp
+++ Blok C/W5/5_6.cpp
@@ -3,6 +3,21 @@
 
 using namespace std;
 
-void vijf_zes(){
+float add(vector<int> list, int size) {
+    if (size <= 0){
+        return 0;
+    }
+    if (size == 1) {
+        return list[0];
+    }
+    else{
+        return list[size-1] + add(list, size-1);
+    }
+}
 
+void vijf_zes() {
+    vector<int> random = {52, 39, 90, 7, 57, 43, 67, 30, 5, 66, 100, 90, 33, 18, 80}; // total = 777 / average = 51.8
+    float total = add(random, random.size());
+    float average = total/random.size();
+    cout << average;
 }
--- Blok C/W5/5_8.cpp
+++ Blok C/W5/5_8.cpp
@@ -1,4 +1,31 @@
+#include "iostream"
+#include "vector"
+#include "string"
+
+using namespace std;
+
+
+
+vector<string> richting(vector<string> map, int size){
+}
+
 
 void vijf_acht(){
-
+    vector<string> map = {  "west", "west", "south", "south", "south", "west",
+                            "west", "west", "north", "north", "north", "north", "north", "west",
+                            "west", "west", "west", "west", "west", "south", "south", "south",
+                            "south", "east", "east", "east", "west", "west", "south", "south",
+                            "south", "west", "west", "north", "north", "north", "north", "north",
+                            "north", "west", "west", "west", "west", "west", "west", "south",
+                            "south", "south", "south", "east", "east", "east", "north", "north",
+                            "north", "north", "north", "north", "north", "north", "north",
+                            "north", "north", "east", "east", "east", "east", "east", "east",
+                            "east", "east", "east", "south", "south", "south", "south", "south",
+                            "west", "west", "west", "east", "east", "east", "east", "east",
+                            "east", "east", "east", "north", "west", "north", "north", "north",
+                            "north", "north", "north", "north", "north", "north", "north"};
+    vector<string> map2 = richting(map, map.size());
+    for (int x = 0; x < map2.size(); x++){
+        cout << map2[x];
+    }
 }
