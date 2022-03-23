6329845be4bbbdcc290e25e33e4bc94f89a6451c
Northie <55920202+northernlakeNL@users.noreply.github.com>
2022-03-23 16:44:36 +0100

    4 6 start

--- Blok C/W4/4_6.cpp
+++ Blok C/W4/4_6.cpp
@@ -1,10 +1,26 @@
 #include "iostream"
 #include "string"
+#include "bitset"
 using namespace std;
 
 
+uint8_t shove(int n, uint8_t ch){
+    if (n < 0){         // naar rechts schuiven
+//        ch
+    }
+    else if (n > 0){    // naar links schuiven
+
+    }
+    else{               // niks doen
+        return ch;
+    }
+}
+
 
 void vier_zes(){
-//    uint8_t ch = 10110001;
-//    uint8_t n;
+    ch = bitset<8>(10110001);
+//    uint8_t ch = 10111000;
+    int n = 3;
+//    int n = -4;
+    cout << shove(n, ch);
 }
